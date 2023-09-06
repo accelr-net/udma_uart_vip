e930db921af0472b9903baedebf8df03ed7bbdcc
kasunb-accelr <kasun.buddhi@accelr.site>
2023-09-06 11:46:01 +0530

    Assigned values to rw using :: operator

--- rtl/uart_vip/uvm_components/cfg_agent/cfg_sequence.svh
+++ rtl/uart_vip/uvm_components/cfg_agent/cfg_sequence.svh
@@ -52,35 +52,27 @@ class cfg_sequence extends uvm_sequence;
         cfg_item = cfg_seq_item::type_id::create("cfg_item");
 
         start_item(cfg_item);
-        //Write sequence here
         cfg_item.cfg_addr_i     <= 5'h09;
         cfg_item.cfg_data_i     <= 32'h01b10306;
-        cfg_item.rw             =  1;
-        // cfg_item.randomize();
+        cfg_item.rw             <= cfg_seq_item::WRITE;
         finish_item(cfg_item);
 
         start_item(cfg_item);
-        //Write sequence here
         cfg_item.cfg_addr_i     <= 5'h04;
         cfg_item.cfg_data_i     <= 32'h1c000934;
-        cfg_item.rw             =  1;
-        // cfg_item.randomize();
+        cfg_item.rw             <= cfg_seq_item::WRITE;
         finish_item(cfg_item);
 
         start_item(cfg_item);
-        //Write sequence here
         cfg_item.cfg_addr_i     <= 5'h05;
         cfg_item.cfg_data_i     <= 32'h00000080;
-        cfg_item.rw             =  1;
-        // cfg_item.randomize();
+        cfg_item.rw             <= cfg_seq_item::WRITE;
         finish_item(cfg_item);
 
         start_item(cfg_item);
-        //Write sequence here
         cfg_item.cfg_addr_i     <= 5'h06;
         cfg_item.cfg_data_i     <= 32'h00000010;
-        cfg_item.rw             =  1;
-        // cfg_item.randomize();
+        cfg_item.rw             <= cfg_seq_item::WRITE;
         finish_item(cfg_item);
     endtask: body
 endclass: cfg_sequence
