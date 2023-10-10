e6a994a7cfcb52aa994ab07c84c2ec7c4bbf41ec
kasunb-accelr <kasun.buddhi@accelr.site>
2023-10-10 15:20:04 +0530

    Stable : Cleaned directory

--- fpga/pulpissimo-genesys2/fpga-settings.mk
+++
@@ -1,7 +0,0 @@
-export BOARD=genesys2
-export XILINX_PART=xc7k325tffg900-2
-export XILINX_BOARD=digilentinc.com:genesys2:part0:1.1
-export FC_CLK_PERIOD_NS=50
-export PER_CLK_PERIOD_NS=100
-export SLOW_CLK_PERIOD_NS=30517
-$(info Setting environment variables for $(BOARD) board)
--- fpga/pulpissimo/tcl/add_sources.tcl
+++
@@ -1,2 +0,0 @@
-# We are using bender for ip managment
-source ../pulpissimo/tcl/generated/compile.tcl
