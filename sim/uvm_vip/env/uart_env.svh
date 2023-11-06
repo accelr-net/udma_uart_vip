4c5f9664aa112520bd98fbd07a79fadfed7b0c9c
kasunb-accelr <kasun.buddhi@accelr.site>
2023-11-06 15:11:50 +0530

    Stable : connected uart_tx

--- sim/uvm_vip/env/uart_env.svh
+++ sim/uvm_vip/env/uart_env.svh
@@ -36,11 +36,11 @@ class uart_env extends uvm_env;
     `uvm_component_utils(uart_env)
     cfg_agent                                       cfg_agnt;
     uart_agent                                      uart_rx_agnt;
-    // uart_agent                                      uart_tx_agnt;
+    uart_agent                                      uart_tx_agnt;
     udma_rx_agent                                   udma_rx_agnt;
     env_config                                      env_configs;
     uart_agent_config                               uart_rx_config;
-    // uart_agent_config                               uart_tx_config;
+    uart_agent_config                               uart_tx_config;
     cfg_agent_config                                cfg_config;
     uart_subscriber                                 sub;
     uart_udma_predictor                             predictor;
@@ -67,7 +67,7 @@ class uart_env extends uvm_env;
         `uvm_info("[ENV]","build_phase",UVM_LOW)
         cfg_agnt            = cfg_agent::type_id::create("cfg_agent",this);
         uart_rx_agnt        = uart_agent::type_id::create("uart_rx_agnt",this);
-        // uart_tx_agnt        = uart_agent::type_id::create("uart_tx_agnt",this);
+        uart_tx_agnt        = uart_agent::type_id::create("uart_tx_agnt",this);
         udma_rx_agnt        = udma_rx_agent::type_id::create("udma_rx_agnt",this);
         // sub                 = uart_subscriber::type_id::create("sub",this);
         predictor           = uart_udma_predictor::type_id::create("predictor",this);
@@ -75,7 +75,7 @@ class uart_env extends uvm_env;
 
         //create configuration objects for agents
         uart_rx_config      = uart_agent_config::type_id::create("uart_rx_config",this);
-        // uart_tx_config      = uart_agent_config::type_id::create("uart_tx_config",this);
+        uart_tx_config      = uart_agent_config::type_id::create("uart_tx_config",this);
         cfg_config          = cfg_agent_config::type_id::create("cfg_config",this);
 
         //udma_tx_agents
@@ -97,15 +97,15 @@ class uart_env extends uvm_env;
         uart_rx_config.period       = env_configs.period;
         uart_rx_config.is_rx_agent  = 1'b1;
 
-        // uart_tx_config.baud_rate    = env_configs.baud_rate;
-        // uart_tx_config.parity_en    = env_configs.parity_en;
-        // uart_tx_config.char_length  = env_configs.char_length;
-        // uart_tx_config.stop_bits    = env_configs.stop_bits;
-        // uart_tx_config.period       = env_configs.period;
-        // uart_tx_config.is_rx_agent  = 1'b0;
+        uart_tx_config.baud_rate    = env_configs.baud_rate;
+        uart_tx_config.parity_en    = env_configs.parity_en;
+        uart_tx_config.char_length  = env_configs.char_length;
+        uart_tx_config.stop_bits    = env_configs.stop_bits;
+        uart_tx_config.period       = env_configs.period;
+        uart_tx_config.is_rx_agent  = 1'b0;
 
         uvm_config_db #(uart_agent_config)::set(this,"uart_rx_agnt*","uart_config",uart_rx_config);
-        // uvm_config_db #(uart_agent_config)::set(this,"uart_tx_agnt*","uart_config",uart_tx_config);
+        uvm_config_db #(uart_agent_config)::set(this,"uart_tx_agnt*","uart_config",uart_tx_config);
     endfunction: build_phase
 
     function void connect_phase(uvm_phase phase);
--- sim/uvm_vip/uart_agent/uart_monitor.svh
+++ sim/uvm_vip/uart_agent/uart_monitor.svh
@@ -110,6 +110,9 @@ class uart_monitor extends uvm_monitor;
                 #this.period;
             end
             #(this.period/2); // wait for stop
+            if(!(rx_config.is_rx_agent)) begin
+                $display("%s uart_tx_transactions %p %s",RED,uart_rx_transaction,WHITE);
+            end
             uart_rx_analysis_port.write(uart_rx_transaction);
         end
     endtask: run_phase
