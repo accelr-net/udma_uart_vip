bff0716d2a860efd9234344c08e6ab80c5e3c0d6
kasunb-accelr <kasun.buddhi@accelr.site>
2023-12-15 12:57:16 +0530

    Stable : added banner to tb_top

--- sim/top/tb_top.sv
+++ sim/top/tb_top.sv
@@ -1,8 +1,37 @@
-/////////////////////////////////////////////////////////////////////////////////////
+// ************************************************************************************************
 //
-// This is for only learn about uart protocal of pulp
+// Copyright(C) 2023 ACCELR
+// All rights reserved.
 //
-//////////////////////////////////////////////////////////////////////////////////////////
+// THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+// ACCELER LOGIC (PVT) LTD, SRI LANKA.
+//
+// This copy of the Source Code is intended for ACCELR's internal use only and is
+// intended for view by persons duly authorized by the management of ACCELR. No
+// part of this file may be reproduced or distributed in any form or by any
+// means without the written approval of the Management of ACCELR.
+//
+// ACCELR, Sri Lanka            https://accelr.lk
+// No 175/95, John Rodrigo Mw,  info@accelr.net
+// Katubedda, Sri Lanka         +94 77 3166850
+//
+// ************************************************************************************************
+//
+// PROJECT      :   UART Verification Env
+// PRODUCT      :   N/A
+// FILE         :   tb_top.sv
+// AUTHOR       :   Kasun Buddhi
+// DESCRIPTION  :   This is top level of the testbench
+//
+// ************************************************************************************************
+//
+// REVISIONS:
+//
+//  Date            Developer     Description
+//  -----------     ---------     -----------
+//  11-Sep-2023      Kasun        creation
+//
+//**************************************************************************************************
 `timescale 1ns/1ns
 import uvm_pkg::*;
 `include "uvm_macros.svh"
