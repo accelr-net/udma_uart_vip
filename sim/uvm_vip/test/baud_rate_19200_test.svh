6b33729e423627b3903a648443389bdad710a301
kasunb-accelr <kasun.buddhi@accelr.site>
2024-01-03 10:07:05 +0530

    Stable : modified the header of the files

--- sim/uvm_vip/test/baud_rate_19200_test.svh
+++ sim/uvm_vip/test/baud_rate_19200_test.svh
@@ -1,15 +1,18 @@
 // ************************************************************************************************
 //
-// Copyright(C) 2023 ACCELR
-// All rights reserved.
+// Copyright 2023, Acceler Logic (Pvt) Ltd, Sri Lanka
 //
-// THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
-// ACCELER LOGIC (PVT) LTD, SRI LANKA.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This copy of the Source Code is intended for ACCELR's internal use only and is
-// intended for view by persons duly authorized by the management of ACCELR. No
-// part of this file may be reproduced or distributed in any form or by any
-// means without the written approval of the Management of ACCELR.
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 // ACCELR, Sri Lanka            https://accelr.lk
 // No 175/95, John Rodrigo Mw,  info@accelr.net
